4e4a23248bbab157f87773df82fcc0ea151d59e4
unknown <maxawzsinger@gmail.com>
2022-02-24 12:07:16 +1100

    changed list to have no bullets, changed font sizes of end screen

--- src/game.js
+++ src/game.js
@@ -46,7 +46,6 @@ function Game() {
     const [screenType, setScreenType] = useState('play');
     const [placement, setPlacement] = useState(0);
     const [playerName, setPlayerName] = useState('');
-    const [databaseRef, setDatabaseRef] = useState({});
     console.log('rerender');
 
 
@@ -84,7 +83,7 @@ function Game() {
     }
 
     useEffect(() => {
-      const app = initializeApp(config);
+      initializeApp(config);
 
 
       loadNewChamp()
@@ -235,52 +234,57 @@ if (screenType == 'play') {
     return (
       <div className="App">
 
-          <div style={{
-              "height": "10vh",
-              "display": "flex",
-              "alignItems": "center",
-              "justifyContent": "space-between",
-              "fontSize": "40px",
-              "fontWeight": 600,
+        <div style={{
+            "height": "10vh",
+            "display": "flex",
+            "alignItems": "center",
+            "justifyContent": "space-between",
+            "fontSize": "40px",
+            "fontWeight": 600,
 
-          }}>
-              <div>{score}</div>
-              <div style={{display: 'flex'}}>
-                  <div style={{width: "50px", opacity: lives >= 3 ? 1 : 0.3}}><svg style={{transform: "scale(2)"}} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ff0000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg></div>
-                  <div style={{width: "50px", opacity: lives >= 2 ? 1 : 0.3}}><svg style={{transform: "scale(2)"}} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ff0000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg></div>
-                  <div style={{width: "50px", opacity: lives >= 1 ? 1 : 0.3}}><svg style={{transform: "scale(2)"}} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ff0000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg></div>
-              </div>
-              </div>
-          <div style={{"height": "70vh"}}>
-          Game Over! The correct answer was {champname}. You made it through {score} out of 1481 splash arts.
-          <div>
-          Highscores:
-          {highscores.map((player) =>
-            <li>{player[0]} : {player[1]}</li>
-          )}
+        }}>
+
+
           </div>
-          </div>
-                            <div style={{"height": "20vh"}}>
-                            <div style={{
-                                "display": "flex",
-                                "height": "20vh",
-                                "justify-content": "center",
-                                "align-items": "center"
-                            }}>
-                            { placement !== 0 &&
-                                <div>
-                                You placed {placement}/10. Enter name for high score.
-                                    <input placeholder=  "enter name" focus style={inputStyle} type="text" onChange={e => setPlayerName(e.target.value)} />
-                                    <button onClick={()=>handleSubmitPlayerHighScore()}>
-                                    Submit high score
-                                    </button>
-                                </div>
-                            }
-                            </div>
-                            </div>
-                            <button onClick={()=>handleNewGame()}>
-                            Start new game
-                            </button>
+      <div style={{"height": "30vh"}}>
+      <h1>
+      Game Over! The correct answer was {champname}. You made it through {score} out of 1481 splash arts.
+      </h1>
+      <div>
+      <h3>
+      Highscores:
+      </h3>
+      <ul style={{listStyleType : 'none'}}>
+
+      {highscores.map((player) =>
+        <li>{player[0]} : {player[1]}</li>
+      )}
+      </ul>
+      </div>
+      </div>
+        <div style={{"height": "20vh"}}>
+        <div style={{
+            "display": "flex",
+            "height": "20vh",
+            "justify-content": "center",
+            "align-items": "center"
+        }}>
+        { placement !== 0 &&
+            <div>
+            You placed {placement}/10. Enter name for high score.
+                <input placeholder=  "enter name" focus style={inputStyle} type="text" onChange={e => setPlayerName(e.target.value)} />
+                <div>
+                <button onClick={()=>handleSubmitPlayerHighScore()}>
+                Submit high score
+                </button>
+                </div>
+            </div>
+        }
+        </div>
+        </div>
+        <button onClick={()=>handleNewGame()}>
+        Start new game
+        </button>
       </div>
     )
   }
